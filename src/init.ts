f33ca99f008081a21efe4b764e305fa423818512
AndryPopovych <an007po@gmail.com>
2026-04-24 18:40:22 +0300

    Update init.ts

--- src/init.ts
+++ src/init.ts
@@ -33,36 +33,29 @@ export async function init(options: {
   // Telegram for macOS has a ton of bugs, including cases, when the client doesn't
   // even response to the "web_app_request_theme" method. It also generates an incorrect
   // event for the "web_app_request_safe_area" method.
-if (options.mockForMacOS) {
-  let firstThemeSent = false;
-  mockTelegramEnv({
-    onEvent(event, next) {
-      if (event.name === 'web_app_request_theme') {
-        // Використовуємо any для tp, щоб уникнути конфліктів типів при ініціалізації
-        let tp: any = {}; 
-
-        if (firstThemeSent) {
-          tp = themeParams.state();
-        } else {
-          firstThemeSent = true;
-          // Отримуємо параметри з лаунч-параметрів
-          tp = retrieveLaunchParams().tgWebAppThemeParams;
+  if (options.mockForMacOS) {
+    let firstThemeSent = false;
+    mockTelegramEnv({
+      onEvent(event, next) {
+        if (event.name === 'web_app_request_theme') {
+          let tp: ThemeParams = {};
+          if (firstThemeSent) {
+            tp = themeParams.state();
+          } else {
+            firstThemeSent = true;
+            tp ||= retrieveLaunchParams().tgWebAppThemeParams;
+          }
+          return emitEvent('theme_changed', { theme_params: tp });
         }
 
-        // Важливо: подія 'theme_changed' очікує змійковий регістр (snake_case).
-        // Оскільки retrieveLaunchParams повертає саме такий формат для tgWebAppThemeParams,
-        // ми просто передаємо його далі.
-        return emitEvent('theme_changed', { theme_params: tp });
-      }
+        if (event.name === 'web_app_request_safe_area') {
+          return emitEvent('safe_area_changed', { left: 0, top: 0, right: 0, bottom: 0 });
+        }
 
-      if (event.name === 'web_app_request_safe_area') {
-        return emitEvent('safe_area_changed', { left: 0, top: 0, right: 0, bottom: 0 });
-      }
-
-      next();
-    },
-  });
-}
+        next();
+      },
+    });
+  }
 
   // Mount all components used in the project.
   backButton.mount.ifAvailable();
